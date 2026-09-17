01585a438d7e1cc315f02dd9615976e65ebe0f91
sebpie26 <pietig@student.chalmers.se>
2026-09-17 11:42:23 +0200

    CHP with heat vent, and biomass generator

--- src/model_0.py
+++ src/model_0.py
@@ -4,7 +4,9 @@ import numpy as np
 import pypsa
 
 
-def conceptual_model(snapshots=168):
+def conceptual_model(model_parameter, snapshots=24):
+
+    mp = model_parameter
 
     n = pypsa.Network()
     n.set_snapshots(range(snapshots))
@@ -19,16 +21,23 @@ def conceptual_model(snapshots=168):
         bus="co2 atmosphere",
     )
 
-    # Biomass
+    # Biomass: Initial biomass is set in the store, and a generator is added to represent the supply of biomass.
     n.add("Carrier", "solid biomass")
     n.add("Bus", "solid biomass", carrier="solid biomass")
     n.add("Store", "solid biomass",
         e_nom=np.inf,
-        e_initial=30000,
+        e_initial=mp["biomass_potential"],
         carrier="solid biomass",
         bus="solid biomass",
-        marginal_cost=30, #biomass price
-    )
+        marginal_cost=30) #biomass price
+    
+    n.add("Generator", "biomass supply",
+        bus="solid biomass",
+        carrier="solid biomass",
+        p_nom_extendable=True,
+        p_nom_max=mp["biomass_potential"],
+        marginal_cost=30) #biomass price
+
 
     # co2 storage
     n.add("Carrier", "co2 captured")
@@ -40,6 +49,7 @@ def conceptual_model(snapshots=168):
         marginal_cost=0,
     )
 
+
     # CHP+CCS plant
     n.add("Carrier", "electricity")
     n.add("Bus", "electricity", carrier="electricity")
@@ -47,14 +57,35 @@ def conceptual_model(snapshots=168):
     n.add("Bus", "heat", carrier="heat")
 
     n.add("Link", "CHP+CCS",
-            bus0="solid biomass", bus1="electricity", bus2="co2 atmosphere", bus3="co2 captured",
-            efficiency=0.6, efficiency2=0.0345, efficiency3=0.1955,
+            bus0="solid biomass", bus1="electricity", bus2="heat", bus3="co2 atmosphere", bus4="co2 captured",
+            efficiency=0.35, efficiency2=0.5, efficiency3=0.0345, efficiency4=0.1955,
             p_nom_extendable=True, capital_cost=10000, marginal_cost=30)
 
 
     # Loads
-    #n.add("Load", "heat load", bus="heat", p_set=100)
+    n.add("Load", "heat load", bus="heat", p_set=100)
     n.add("Load", "electricity load", bus="electricity", p_set=100)
+
+
+    # Heat vent and electricity vent to not violate the energy balance (chp out = load).
+    n.add("Generator", "heat vent",
+        bus="heat",
+        carrier="heat",
+        p_nom_extendable=True,
+        p_max_pu=0,
+        p_min_pu=-1,
+    )
+
+    n.add("Generator", "electricity vent",
+        bus="electricity",
+        carrier="electricity",
+        p_nom_extendable=True,
+        p_max_pu=0,
+        p_min_pu=-1,
+        marginal_cost=-10,  # Electricity still generates income
+    )
+
+
 
 
     return n
@@ -62,12 +93,17 @@ def conceptual_model(snapshots=168):
 
 if __name__ == "__main__":
 
-    model = conceptual_model()
+    model_parameter = {
+        "biomass_potential": 1000   #MWh of biomass available in every snapshot (generator) and initially (store)
+    }
+
+    model = conceptual_model(model_parameter)
 
     model.optimize()
 
     model.stores_t.e.plot()
     model.links_t.p0.plot()
+    model.generators_t.p.plot()
 
 
     plt.show()
