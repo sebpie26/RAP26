71288e2e0e0c39c2cbcc1391448c0c3aeb5673f6
sebpie26 <pietig@student.chalmers.se>
2026-09-17 09:59:52 +0200

    commit

---
+++ src/model_0.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import pypsa
+
+
+def conceptual_model(snapshots=168):
+
+    n = pypsa.Network()
+    n.set_snapshots(range(snapshots))
+
+    # Atmosphere
+    n.add("Carrier", "co2", co2_emissions=-1.0)
+    n.add("Bus", "co2 atmosphere", carrier="co2", unit="t_co2")
+    n.add("Store", "co2 atmosphere",
+        e_nom=np.inf,
+        e_min_pu=-1,
+        carrier="co2",
+        bus="co2 atmosphere",
+    )
+
+    # Biomass
+    n.add("Carrier", "solid biomass")
+    n.add("Bus", "solid biomass", carrier="solid biomass")
+    n.add("Store", "solid biomass",
+        e_nom=np.inf,
+        e_initial=30000,
+        carrier="solid biomass",
+        bus="solid biomass",
+        marginal_cost=30, #biomass price
+    )
+
+    # co2 storage
+    n.add("Carrier", "co2 captured")
+    n.add("Bus", "co2 captured", carrier="co2 captured", unit="t_co2")
+    n.add("Store", "co2 captured",
+        e_nom=np.inf,
+        carrier="co2 captured",
+        bus="co2 captured",
+        marginal_cost=0,
+    )
+
+    # CHP+CCS plant
+    n.add("Carrier", "electricity")
+    n.add("Bus", "electricity", carrier="electricity")
+    n.add("Carrier", "heat")
+    n.add("Bus", "heat", carrier="heat")
+
+    n.add("Link", "CHP+CCS",
+            bus0="solid biomass", bus1="electricity", bus2="co2 atmosphere", bus3="co2 captured",
+            efficiency=0.6, efficiency2=0.0345, efficiency3=0.1955,
+            p_nom_extendable=True, capital_cost=10000, marginal_cost=30)
+
+
+    # Loads
+    #n.add("Load", "heat load", bus="heat", p_set=100)
+    n.add("Load", "electricity load", bus="electricity", p_set=100)
+
+
+    return n
+
+
+if __name__ == "__main__":
+
+    model = conceptual_model()
+
+    model.optimize()
+
+    model.stores_t.e.plot()
+    model.links_t.p0.plot()
+
+
+    plt.show()
